8c8db9b740c941ae1063c377f0ff75bfa67a3c65
John Hadden <59323260+jhadden04@users.noreply.github.com>
2020-12-01 18:42:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import praw
 import time
-reddit = praw.Reddit(client_id='09tgNAaa_kchTA',
-                     client_secret='EqtcxF2IqjbUwT5uh3sSGXmQ8b0wfg',
-                     user_agent='a reddit instance',
-                     username='Alert-Target-8166',
-                     password='poggers', )
-def subspam():
-    misctext = "a"  # miscellaneous message
-    # customised messages for each subreddit
-    textamong = "a"
-    textgaming = "a"
-    textmobile = "a"
-    textshopping = "a"
 
-    subname = 'AmongUs+gaming+MobileGaming+Shopping' # obviously you can add more subreddits to this
+reddit = praw.Reddit(client_id='01tuOpG6GAHSIQ',
+                     client_secret='0nQyf-5h7UY4RfzZD0FytbICzpN-Iw',
+                     user_agent='a reddit instance',
+                     username='ImpostermeOffical',
+                     password='Project2020')
+
+
+def subspam():
+    misctext = "Don’t miss out on the limited edition release of the Crewmate Plushy, limited quantities remaining! Get yours today at Imposterme.com"  # miscellaneous message
+    # customised messages for each subreddit
+    textamong = "Don’t miss out on the limited edition release of the Crewmate Plushy, limited quantities remaining! Get yours today at Imposterme.com"
+    textgaming = "Don’t miss out on the limited edition release of the Crewmate Plushy, limited quantities remaining! Get yours today at Imposterme.com"
+    textmobile = "Don’t miss out on the limited edition release of the Crewmate Plushy, limited quantities remaining! Get yours today at Imposterme.com"
+    textshopping = "Don’t miss out on the limited edition release of the Crewmate Plushy, limited quantities remaining! Get yours today at Imposterme.com"
+
+    subname = 'AmongUs+gaming+MobileGaming+Shopping'  # obviously you can add more subreddits to this
     usedusernames = []
     for comment in reddit.subreddit(subname).stream.comments():
         name = comment.author.name
@@ -27,7 +30,7 @@ def subspam():
                 text = textgaming
             elif comment.subreddit.display_name == "MobileGaming":
                 text = textmobile
-            elif comment.subreddit.display_name == "Shopping":     # if you can add more of these personalised ones
+            elif comment.subreddit.display_name == "Shopping":  # if you can add more of these personalised ones
                 text = textshopping
             else:
                 text = misctext
@@ -37,6 +40,5 @@ def subspam():
         usedusernames.append(name)
         print(f'u/{name} r/{comment.subreddit.display_name}')
         time.sleep(40)
-
-
 subspam()
+
