efd4082ddcfd79c18f70625dd4112200b4901882
John Hadden <59323260+jhadden04@users.noreply.github.com>
2020-12-02 15:15:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,6 @@ reddit = praw.Reddit(client_id='01tuOpG6GAHSIQ',
 
 def subspam():
     misctext = "Don’t miss out on the limited edition release of the Crewmate Plushy, limited quantities remaining! Get yours today at Imposterme.com"  # miscellaneous message
-    # customised messages for each subreddit
-    textamong = "Don’t miss out on the limited edition release of the Crewmate Plushy, limited quantities remaining! Get yours today at Imposterme.com"
-    textgaming = "Don’t miss out on the limited edition release of the Crewmate Plushy, limited quantities remaining! Get yours today at Imposterme.com"
-    textmobile = "Don’t miss out on the limited edition release of the Crewmate Plushy, limited quantities remaining! Get yours today at Imposterme.com"
-    textshopping = "Don’t miss out on the limited edition release of the Crewmate Plushy, limited quantities remaining! Get yours today at Imposterme.com"
 
     subname = 'AmongUs+gaming+MobileGaming+Shopping'  # obviously you can add more subreddits to this
     usedusernames = []
@@ -24,16 +19,7 @@ def subspam():
         if name in usedusernames:
             continue
         try:
-            if comment.subreddit.display_name == "AmongUs":
-                text = textamong
-            elif comment.subreddit.display_name == "gaming":
-                text = textgaming
-            elif comment.subreddit.display_name == "MobileGaming":
-                text = textmobile
-            elif comment.subreddit.display_name == "Shopping":  # if you can add more of these personalised ones
-                text = textshopping
-            else:
-                text = misctext
+            text = misctext
             reddit.redditor(name).message(title, text)
         except:
             continue
