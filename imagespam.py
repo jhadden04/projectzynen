e85a88b0b7714586013cc1e2a56094d088d1fd1b
John Hadden <59323260+jhadden04@users.noreply.github.com>
2020-12-02 17:45:32 +0000

    Update imagespam.py

--- imagespam.py
+++ imagespam.py
@@ -1,19 +1,25 @@
 import praw
 import time
-
+import random
 reddit = praw.Reddit(client_id='01tuOpG6GAHSIQ',
                      client_secret='0nQyf-5h7UY4RfzZD0FytbICzpN-Iw',
                      user_agent='a reddit instance',
                      username='ImpostermeOffical',
                      password='Project2020')
+
+
 def imageposter():
     while True:
-        subreddit = reddit.subreddit('AmongUs+gaming+MobileGaming+Shopping')
-        titles = ["change this"] # you must change this to your desired title
+        subname = 'AmongUs+gaming+MobileGaming+Shopping'
+        subreddits = ["AmongUs", "gaming", "MobileGaming", "Shopping"]
+        x = random.randint(0, (len(subreddits)-1))
+        subreddit = reddit.subreddit(subreddits[x])
+        titles = ["change this"]  # you must change this to your desired title
         reddit.validate_on_submit = True
         images = ["C:\\Users\\jhadd\\Pictures\\discord bot.PNG"]  # you must change this to the path on your computer, and use double slashes
         subreddit.submit_image(titles[0], images[0])
         print(f'Image posted')
         time.sleep(3600)
+
 
 imageposter()
