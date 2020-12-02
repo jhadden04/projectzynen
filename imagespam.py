713dcd3946f113302613de19db825262616fa8f2
John Hadden <59323260+jhadden04@users.noreply.github.com>
2020-12-02 17:51:49 +0000

    Update imagespam.py

--- imagespam.py
+++ imagespam.py
@@ -6,18 +6,19 @@ reddit = praw.Reddit(client_id='01tuOpG6GAHSIQ',
                      user_agent='a reddit instance',
                      username='ImpostermeOffical',
                      password='Project2020')
-
-
 def imageposter():
     while True:
         subname = 'AmongUs+gaming+MobileGaming+Shopping'
         subreddits = ["AmongUs", "gaming", "MobileGaming", "Shopping"]
         x = random.randint(0, (len(subreddits)-1))
         subreddit = reddit.subreddit(subreddits[x])
-        titles = ["Don’t miss out on the limited edition release of the Crewmate Plushy, limited quantities remaining! Get yours today at Imposterme.com"]  # you must change this to your desired title
+        titles = ["change this"]  # you must change this to your desired title
         reddit.validate_on_submit = True
-        images = ["C:\\Users\\thein\\Pictures\\snapchatad.jpg"]  # you must change this to the path on your computer, and use double slashes
-        subreddit.submit_image(titles[0], images[0])
+        images = ["C:\\Users\\jhadd\\Pictures\\discord bot.PNG"]  # you must change this to the path on your computer, and use double slashes
+        try: 
+            subreddit.submit_image(titles[0], images[0])
+        except: 
+            continue
         print(f'Image posted')
         time.sleep(3600)
 
